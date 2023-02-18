a9f0860bbf4f90c40ba044d38067018c47ae09ec
tw295 <tw295@duke.edu>
2023-02-18 00:37:03 -0500

    update

--- synopsys-setup.sh
+++
@@ -1,3 +0,0 @@
-export VCS_HOME=/usr/pkg/synopsys/vcs/Q-2020.03-SP2-2
-export SNPSLMD_LICENSE_FILE=/usr/pkg/synopsys/vcs/Q-2020.03-SP2-2/admin/license/synopsys.lic
-export PATH=$PATH:/usr/pkg/synopsys/vcs/Q-2020.03-SP2-2/bin:.
